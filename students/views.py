a923749ff07d47dd5c14a5bd2e075f1eaeeafad3
Aleks_work <shurikkiev@ukr.net>
2018-05-04 17:35:59 +0300

    chapter 6 - dynamic students list

--- students/views.py
+++ students/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,7 +6,24 @@ from django.http import HttpResponse
 # Views for Students
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u"Андрій",
+         'last_name': u'Корост',
+         'ticket': 257618,
+         'image': 'img/me.jpeg'},
+        {'id': 2,
+         'first_name': u'Тарас',
+         'last_name': u'Притула',
+         'ticket': 255671,
+         'image': 'img/piv.png'},
+        {'id': 3,
+         'first_name': u'Віталій',
+         'last_name': u'Подоба',
+         'ticket': 359455,
+         'image': 'img/podoba3.jpg'}
+    )
+    return render(request, 'students/students_list.html', {'students': students})
 
 
 def students_add(request):
