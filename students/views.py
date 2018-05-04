e601964bb34eababaca237bd5f1cf0ba4c2c1c2e
Aleks_work <shurikkiev@ukr.net>
2018-05-04 14:23:18 +0300

    add dynamic urls, updated urls and views 04/05

--- students/views.py
+++ students/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here. lets try clone to front change author
+
+# Views for Students
+
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+
+
+def students_add(request):
+    return HttpResponse('<h1>Students Add Form</h1>')
+
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Student %s Edit Form</h1>' % sid)
+
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Student %s Delete Form</h1>' % sid)
+
+
+# Views for Groups
+
+def groups_list(request):
+    return HttpResponse('<h1>Groups Listing</h1>')
+
+
+def groups_add(request):
+    return HttpResponse('<h1>Group Add Form</h1>')
+
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Group %s Edit Form</h1>' % gid)
+
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Group %s Delete Form</h1>' % gid)
